da421a220cbd1a7007ce6710929371731579c6bd
Ahmed Magdy Abd-alzaher <ahmed.magdy.abd.alzaher@gmail.com>
2018-02-01 04:05:02 +0200

    Changes Has been Submitted

--- app/src/main/java/com/musical/MainActivity.java
+++ app/src/main/java/com/musical/MainActivity.java
@@ -1,23 +1,45 @@
 package com.musical;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    LinearLayout leftRL;
-    DrawerLayout drawerLayout;
+    ListView songList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.drawer);
-        leftRL = (LinearLayout) findViewById(R.id.whatYouWantInLeftDrawer);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-    }
-    public void onOpenLeftDrawer(View view) {
-        drawerLayout.openDrawer(leftRL);
+        setContentView(R.layout.activity_main);
+        String songName = getResources().getString(R.string.song_name_placeholder);
+        String songAuthor = getResources().getString(R.string.song_author_placeholder);
+        int songsCount = 50;
+        int i = 0;
+        ArrayList<String[]> songsList = new ArrayList<>();
+        while( i < songsCount){
+            String[] song = new String[]{ songName, songAuthor};
+            songsList.add(i, song);
+            i++;
+        }
+        songList = (ListView) findViewById(R.id.songs_list);
+        songList.setAdapter(new SongsAdapter(this, songsList ));
+
+
+        FloatingActionButton addSongsFab = (FloatingActionButton) findViewById(R.id.add_songs);
+        addSongsFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addSongs = new Intent(getApplicationContext(), AddSongs.class);
+                startActivity(addSongs);
+            }
+        });
+
     }
 }
