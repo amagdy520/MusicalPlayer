ca9862a3447d207218fdbddee5a24fa2f632a53a
Ahmed Magdy Abd-alzaher <ahmed.magdy.abd.alzaher@gmail.com>
2018-02-03 01:33:15 +0200

    Changes Have Been Pushed

--- app/src/main/java/com/musical/Songs/SongDetails.java
+++ app/src/main/java/com/musical/Songs/SongDetails.java
@@ -2,6 +2,7 @@ package com.musical.Songs;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -16,6 +17,14 @@ public class SongDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.song_details);
+        FloatingActionButton addSongsFab = (FloatingActionButton) findViewById(R.id.add_songs);
+        addSongsFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addSongs = new Intent(getApplicationContext(), AddSongs.class);
+                startActivity(addSongs);
+            }
+        });
     }
 
     public void openRelatedSong(View view){
